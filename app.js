1b2b623dddbef8c9cf6c38ba521db69b9d6ae594
Jacob <jacobo593@gmail.com>
2020-04-04 10:36:28 -0400

    Reading input data

--- app.js
+++ app.js
@@ -10,7 +10,29 @@ var budgetController = (function() {
 //UI CONTROLLER
 var UIController = (function() {
 
-    //some code
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+
+        getInput: function(){
+
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+
+    }
 
 })();
 
@@ -18,15 +40,21 @@ var UIController = (function() {
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UIController.getDOMstrings();
+
     var ctrlAddItem = function() {
          // 1. Get the filed input data
+
+        var input = UIController.getInput();
+        console.log(input)
+
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
     }
 
-   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+   document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
    document.addEventListener('keypress', function(event) {
         
