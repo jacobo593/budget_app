129c4522f37f1bb7b98fa4528096434d39251dd8
Jacob <jacobo593@gmail.com>
2020-04-04 11:47:29 -0400

    saving work

--- app.js
+++ app.js
@@ -78,6 +78,44 @@ var UIController = (function() {
             };
 
         },
+        addListItem: function(obj, type) {
+            var html, newHtml;
+
+            // Create HTML string with placeholder text
+            if(type === 'inc'){
+                html = (
+                    '<div class="item clearfix" id="income-%id%">'+
+                        '<div class="item__description">%description%</div>'+
+                            '<div class="right clearfix">'+
+                                '<div class="item__value">%value%</div>'+
+                                '<div class="item__delete">'+
+                                '<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'
+                );
+            }else if(type === 'exp') {
+                html = (
+                    '<div class="item clearfix" id="income-%id%">'+
+                        '<div class="item__description">%description%</div>'+
+                            '<div class="right clearfix">'+
+                                '<div class="item__value">%value%</div>'+
+                                '<div class="item__delete">'+
+                                '<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>'+
+                            '</div>'+
+                        '</div>'+
+                '</div>'
+                );
+            }
+
+            // Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the HTML into the DOM
+
+        },
         getDOMstrings: function() {
             return DOMstrings;
         }
