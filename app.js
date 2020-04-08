3ef29978ffd69a482d458e86066ea6fb724cd44b
Jacob <jacobo593@gmail.com>
2020-04-08 18:06:40 -0400

    update percentages controller

--- app.js
+++ app.js
@@ -254,6 +254,16 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     };
 
+    var updatePercentages = function() {
+
+        // 1. Calculate percentages
+
+        // 2. Read percentages from the budget controller
+
+        // 3. Update the UI with the new percentages
+
+    };
+
     var ctrlAddItem = function() {
         var input, newItem;
 
@@ -273,6 +283,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 5. Calculate and update budget
             updateBudget();
+
+            // 6. Calculate and update percentages
+            updatePercentages();
 
         }
 
@@ -299,6 +312,8 @@ var controller = (function(budgetCtrl, UICtrl) {
             // 3. Update and show the new budget
             updateBudget();
 
+            // 4. Calculate and update percentages
+            updatePercentages();
         }
 
     };
